5a9acdf94427dd521e4c2699caa8bf5715eb9507
Brendan Bullen <bullenb@gmail.com>
2020-10-19 12:35:13 +0100

    Fix predis cache config for AWS encryption requirements

--- src/Managers/PredisCluster.php
+++ src/Managers/PredisCluster.php
@@ -29,11 +29,11 @@ class PredisCluster extends Predis
         }
 
         if (isset($connections[0]['password'])) {
-            $options['password'] = $connections[0]['password'];
+            $options['parameters']['password'] = $connections[0]['password'];
         }
 
         if (isset($connections[0]['scheme'])) {
-            $options['scheme'] = $connections[0]['scheme'];
+            $options['parameters']['scheme'] = $connections[0]['scheme'];
         }
 
         $this->client = new Client($connections, $options);
